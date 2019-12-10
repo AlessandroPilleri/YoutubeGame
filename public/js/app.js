dcb02aaca3af4f13348b2c1fb0624c1cf7448204
AlessandroPilleri <alessandro.pilleri@fermi.mo.it>
2019-12-10 13:19:41 +0100

    Synthetize bars number

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,8 @@ const url = document.querySelector('#url');
 const start = document.querySelector('#start');
 const audioTag = document.querySelector('#audio');
 const canvas = document.querySelector('#canvas');
+const fps = document.querySelector('#fps');
+
 
 start.addEventListener('click', () => {
     let tmp = url.value.split('?');
@@ -13,24 +15,26 @@ start.addEventListener('click', () => {
 
     let context = new AudioContext();
     let analyser = context.createAnalyser();
+    analyser.fftSize = 32;
+    console.log(analyser.frequencyBinCount);
 
     let frequencyData = new Uint8Array(analyser.frequencyBinCount);
-    let w = 4000/(frequencyData.length/1.5);
+    console.log(frequencyData.length);
+    let w = 1000/frequencyData.length;
     let precedentFrequency = new Uint8Array(frequencyData);
-    console.log(precedentFrequency);
     let content = canvas.getContext('2d');
 
     let world = planck.World();
     console.log(world);
 
     let bars = [];
-    for (let index = 0; index < frequencyData.length/1.5; index++) {
+    for (let index = 0; index < frequencyData.length; index++) {
         let body = world.createBody({
             type: 'dynamic',
-            position: planck.Vec2(index*w*2, 0)
+            position: planck.Vec2(w/2 + index*w, 0)
         });
         body.createFixture({
-            shape: planck.Box(w, 400/2)
+            shape: planck.Box(w/2, 400/2)
         });
 
         bars.push(body);
@@ -40,7 +44,7 @@ start.addEventListener('click', () => {
 
     let x = false;
     let start = -1;
-
+    let count = 0;
     function update() {
         let now = new Date().getTime();
         if (start === -1) {
@@ -49,38 +53,29 @@ start.addEventListener('click', () => {
         let delta = now - start;
         start = now;
         analyser.getByteFrequencyData(frequencyData);
-        content.clearRect(0, 0, 4000, 400);
+        content.clearRect(0, 0, 1000, 400);
         content.fillStyle = 'black';
-        content.fillRect(0, 0, 4000, 400);
+        content.fillRect(0, 0, 1000, 400);
 
-        for (let index = 0; index < frequencyData.length/1.5; index++) {
-
+        for (let index = 0; index < frequencyData.length; index++) {
             let direction = frequencyData[index] - precedentFrequency[index];
             let f = bars[index].getWorldVector(planck.Vec2(0, direction * 1000));
             let p = bars[index].getWorldPoint(planck.Vec2(0, 0));
             bars[index].applyLinearImpulse(f, p, true);
+            let fixture = bars[index].getFixtureList();
+            content.beginPath();
+            fixture.m_shape.m_vertices.forEach( (vertex) => {
+                content.lineTo(vertex.x + bars[index].getPosition().x, 400 - vertex.y - bars[index].getPosition().y);
+
+            });
+            content.strokeStyle = "red";
+            content.lineWidth = "2";
+            content.stroke();
+            content.closePath();
             precedentFrequency[index] = frequencyData[index];
         }
         world.step(delta);
-
-        for (let body = world.getBodyList(); body; body = body.getNext()) {
-            for (let fixture = body.getFixtureList(); fixture; fixture = fixture.getNext()) {
-                content.beginPath();
-                fixture.m_shape.m_vertices.forEach( (vertex) => {
-                    content.lineTo(vertex.x + body.getPosition().x, 400 - vertex.y - body.getPosition().y);
-
-                });
-                content.strokeStyle = "red";
-                content.lineWidth = "2";
-                content.stroke();
-                content.closePath();
-
-                if (body.getPosition().y > 400) {
-                    console.log(body.getPosition());
-                    x = true;
-                }
-            }
-        }
+        count++;
         requestAnimationFrame(update);
     }
 
@@ -89,5 +84,9 @@ start.addEventListener('click', () => {
         source.connect(analyser);
         analyser.connect(context.destination);
         update();
+        setInterval( () => {
+            fps.innerHTML = count;
+            count = 0;
+        }, 1000)
     })
 });
